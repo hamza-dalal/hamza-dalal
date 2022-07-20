71900044f811d9f0f6444ba7aa384c3ade17243e
hamza-dalal <65228870+hamza-dalal@users.noreply.github.com>
2022-07-21 00:53:47 +0530

    Add files via upload

---
+++ C-C++/String_Handling/String Copy.c
@@ -0,0 +1,15 @@
+#include<math.h>
+#include<conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+main()
+{
+	char x[10]="Somaiya",m[10],a[10];
+	char z[12]="Vidyavihar";
+	system("cls");
+	strcpy(a,z);
+	printf("%s - %s, ",x,a);
+	strcpy(m,"Ghatkopar");
+	printf("%s",m);
+}
+
