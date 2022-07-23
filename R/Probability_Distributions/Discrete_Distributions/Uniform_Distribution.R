056b822eb88cdc6a4cf0085bec068481e7d3ad1f
hamza-dalal <65228870+hamza-dalal@users.noreply.github.com>
2022-07-24 00:23:03 +0530

    Create Uniform_Distribution.R

---
+++ R/Probability_Distributions/Discrete_Distributions/Uniform_Distribution.R
@@ -0,0 +1,7 @@
+# Discrete Uniform Distribution
+
+# Discrete uniform has same probability for all the observations. Hence only sense is to create random numbers through this distribution in R.
+
+library(purrr)
+# To generate discrete random numbers
+x <- rdunif(100, 50); x
