877c4c429111e9aba4700974829a843806edbb93
hamza-dalal <65228870+hamza-dalal@users.noreply.github.com>
2022-07-26 22:32:22 +0530

    Update Uniform_Distribution.R

--- R/Probability_Distributions/Continous_Distributions/Uniform_Distribution.R
+++ R/Probability_Distributions/Continous_Distributions/Uniform_Distribution.R
@@ -1,14 +1,14 @@
 # Continuous Uniform Distribution
 
 # Probability Distribution
-x <- dunif(25:30, 20, 50); x
+x <- dunif(25:30, min = 20, max = 50); x
 
 # Cumulative Distribution
-y <- punif(5:15, 0, 20); y
+y <- punif(5:15, min = 0, max = 20); y
 
 # Quantiles
-z <- qunif(seq(0.2, 0.8, by = 0.1), 50, 100); z
+z <- qunif(seq(0.2, 0.8, by = 0.1), min = 50, max = 100); z
 
 # Generate random numbers
-a <- runif(150, 10, 11); a
+a <- runif(150, min = 10, max = 11); a
 plot(a)
