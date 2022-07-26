7a66ab91b179f7d3b58f03e86a10f959ff0d7309
hamza-dalal <65228870+hamza-dalal@users.noreply.github.com>
2022-07-27 03:25:56 +0530

    Create student-t_Distribution.R

---
+++ R/Probability_Distributions/Continous_Distributions/student-t_Distribution.R
@@ -0,0 +1,17 @@
+# Chi Square Distribution
+
+# Probability Distribution
+x <- dt(-5:10, df = 2); x
+plot(x)
+
+# Cumulative Distribution
+y <- pt(0, df = 2); y
+plot(y)
+
+# Quantiles
+z <- qt(0.6, df = 5); z
+
+# Generate random numbers
+a <- rt(120, df = 2); a
+plot(a)
+hist(a, breaks = 25)
