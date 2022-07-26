684058215f976a2ba6610dd4b3837b1a66c759a9
hamza-dalal <65228870+hamza-dalal@users.noreply.github.com>
2022-07-27 03:25:46 +0530

    Create Beta_Distribution.R

---
+++ R/Probability_Distributions/Continous_Distributions/Beta_Distribution.R
@@ -0,0 +1,16 @@
+# Beta Distribution
+
+# Probability Distribution
+x <- dbeta(seq(0, 1, by = 0.1), shape1 = 2, shape2 = 5); x
+plot(x)
+
+# Cumulative Distribution
+y <- pbeta(0.3, shape1 = 1, shape2 = 10); y
+
+# Quantiles
+z <- qbeta(0.63, shape1 = 1, shape2 = 3); z
+
+# Generate random numbers
+a <- rbeta(120, shape1 = 1, shape2 = 7); a
+plot(a)
+hist(a, breaks = 25)
